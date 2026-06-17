e4b03760f64af08525899a88dfe616417d83a072
Laurence Bichlbauer <lth.bichlbauer04@mci4me.at>
2026-06-17 17:37:00 +0200

    Abgabe: Objektorientierung

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.ekg_data import EKGdata
 from src.read_data import load_person_data, get_name_to_id
 
 st.set_page_config(
-    page_title="EKG Analyse Sheesh",
+    page_title="EKG Analyse Sheesh :)",
     page_icon="❤️",
     #layout="wide"
     layout="centered"
@@ -14,7 +14,7 @@ st.set_page_config(
 
 st.title("EKG Analyse")
 
-
+st.divider()
 
 person_data = load_person_data()
 name_to_id = get_name_to_id(person_data)
@@ -25,7 +25,7 @@ selected_name = st.selectbox("Person auswählen", list(name_to_id.keys()))
 
 selected_id = name_to_id[selected_name]
 
-
+st.divider()
 
 person = Person.load_by_id(selected_id)
 ekg_data = EKGdata.load_by_id(selected_id)
@@ -49,7 +49,7 @@ st.divider()
 st.subheader("EKG-Daten")
 
 threshold = st.slider(
-        "Peak-Schwellenwert",
+        "Peak-Schellenwert",
         min_value=240,
         max_value=400,
         value=340,
